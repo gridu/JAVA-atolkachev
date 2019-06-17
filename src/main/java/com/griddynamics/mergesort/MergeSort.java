7a658978c53bb609203970102da7c9ba38c075d1
atolkachev <atolkachev@griddynamics.comn>
2019-06-18 01:45:29 +0400

    - init

---
+++ src/main/java/com/griddynamics/mergesort/MergeSort.java
@@ -0,0 +1,24 @@
+package com.griddynamics.mergesort;
+
+import java.util.Comparator;
+
+import static java.util.Comparator.naturalOrder;
+
+public class MergeSort {
+
+    /**
+     * Default comparator between strings.
+     */
+    public static Comparator<String> defaultcomparator = naturalOrder();
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void mergeSort(int[] array, int arrayLength) {
+
+    }
+
+}
---
+++ src/main/java/com/griddynamics/mergesort/exception/IncorrectFileNameException.java
@@ -0,0 +1,8 @@
+package com.griddynamics.mergesort.exception;
+
+public class IncorrectFileNameException extends Exception {
+
+    public IncorrectFileNameException(String errorMessage, Throwable cause) {
+        super(errorMessage, cause);
+    }
+}
